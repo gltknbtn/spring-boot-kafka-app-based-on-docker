90947894277a69ced56ce8d0b2e786bfc294283a
gltknbtn <gltknbtn@gmail.com>
2020-11-16 01:24:02 +0300

    docker host name integrated

--- springboot-kafka-consumer/src/main/java/com/gltknbtn/SolrConfig.java
+++ springboot-kafka-consumer/src/main/java/com/gltknbtn/SolrConfig.java
@@ -22,7 +22,7 @@ public class SolrConfig {
 
     @Bean
     public SolrClient solrClient() {
-        SolrClient solrClient = new HttpSolrClient.Builder("http://"+env.getProperty("SOLR_HOSTNAME") != null ? env.getProperty("SOLR_HOSTNAME") : "localhost" +":8983/solr").build();
+        SolrClient solrClient = new HttpSolrClient.Builder("http://" +env.getProperty("SOLR_HOSTNAME") +":8983/solr").build();
         return solrClient;
     }
 
