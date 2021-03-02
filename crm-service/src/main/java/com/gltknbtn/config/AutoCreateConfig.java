b12fdbadb09300580ba3db56ae8348f719db223a
gltknbtn <gltknbtn@gmail.com>
2021-03-02 22:34:53 +0300

    kafkaAdminClient conf added

--- crm-service/src/main/java/com/gltknbtn/config/AutoCreateConfig.java
+++ crm-service/src/main/java/com/gltknbtn/config/AutoCreateConfig.java
@@ -1,14 +1,36 @@
 package com.gltknbtn.config;
 
+import com.gltknbtn.producer.LoggingProducer;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @Profile("local")
 public class AutoCreateConfig {
+
+    private final Logger logger = Logger.getLogger(AutoCreateConfig.class);
+
+    @Value(value = "${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin(){
+        logger.info("bootstrapAddress for crm-service: " + bootstrapAddress);
+
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
 
     @Bean
     public NewTopic libraryEvents(){
